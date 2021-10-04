b5ba1a6c4d8752210686071828364de68bb312b9
Oleksandr Yemets <alexxandryemets@gmail.com>
2021-10-04 19:09:07 +0300

    2nd version of Homework 3 done

---
+++ OOP/Homeworks/Homework3/Program.cs
@@ -0,0 +1,24 @@
+﻿//TASK_2 + TASK_3 of Homework #3 
+namespace Task3
+{
+    
+    internal class ProgramTask3
+    {
+         public static void Main()
+         {
+             testingClassConverter.testingFunctionConverter();
+         }
+    }
+}
+namespace Task2
+{
+    internal class Program
+    {
+        public const string PathToFile = "/home/oleksandr/Labs/OOP/Homeworks/Homework3/students.txt";
+
+       // public static void Main()
+       // {
+       //     testingClassStudents.testingFunctionStudents();
+       // }
+    }
+}
